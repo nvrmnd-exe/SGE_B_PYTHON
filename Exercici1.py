43d06f2e7ccef5aa8a76438127d811ee1844ca5f
nvrmnd-exe <yoonlopezluis@gmail.com>
2024-11-07 11:33:33 +0100

    Afegit exercici 1

---
+++ Exercici1.py
@@ -0,0 +1,14 @@
+'''
+Crear dues variables i comparar-les indicant quina és més gran, quina és més petita o si son iguals.
+'''
+
+#Crea les variables
+var1 = 1803
+var2 = 9293
+
+if var1 == var2:
+    print(var1 + " és igual que " + var2)
+elif var1 > var2:
+    print(var1 + " és més gran que " + var2)
+else:
+    print(var1 + " és més petit que " + var2)
