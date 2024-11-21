a71ca8f3ed4e65f976f005eaad7e3bb56d529251
nvrmnd-exe <yoonlopezluis@gmail.com>
2024-11-21 12:01:05 +0100

    Acabat exercici 2

--- Activitat3_Bucles/Exercici1.py
+++ Activitat3_Bucles/Exercici1.py
@@ -1,5 +1,11 @@
+#While
 i = 1
-
 while i <= 10:
+    print(i)
+    i = i + 1
+
+#For
+ll = [1,2,3,4,5,6,7,8,9,10]
+for i in ll:
     print(i)
     i = i + 1
---
+++ Activitat3_Bucles/Exercici2.py
@@ -0,0 +1,6 @@
+#Estableix el rang de 10 primers nums (0-9)
+num = range(10)
+
+##Mostra el numero si està al rang
+for i in num:
+    print(i)
