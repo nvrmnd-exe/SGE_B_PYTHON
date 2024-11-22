e2a563ca8394532b5663a0e59ca5f4fdd6bf6bda
nvrmnd-exe <yoonlopezluis@gmail.com>
2024-11-22 12:17:45 +0100

    Acabat ejs

---
+++ Activitat3_Bucles/Exercici6.py
@@ -0,0 +1,10 @@
+#Variables
+num = range(100)
+i = 0
+suma = 0
+
+#Suma a "suma" el número 
+while i in num:
+    suma = suma + i
+    i = i + 1
+print(suma)
---
+++ Activitat3_Bucles/Exercici7.py
@@ -0,0 +1,5 @@
+num = input("Indica un número: ")
+rang = range(100, 400)
+
+if num not in rang:
+    print(f"{num} està entre 100 i 400")
